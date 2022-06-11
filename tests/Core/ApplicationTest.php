1eb592298c7ad154998cf9103b62b8e9aceb1df0
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-12 00:38:59 +0100

    PHPUnit

--- tests/Core/ApplicationTest.php
+++ tests/Core/ApplicationTest.php
@@ -4,5 +4,8 @@ namespace Tests\Core;
 
 final class ApplicationTest extends TestCase
 {
-    
+    public function test_foo()
+    {
+        $this->assertTrue(true);
+    }
 }
---
+++ tests/Core/TestCase.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Core;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    
+}
