89e4da55711ed773eaa29279180b78b157b713fe
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-12 01:15:42 +0100

    index.php

--- public/index.php
+++ public/index.php
@@ -7,17 +7,3 @@ require PROJECT_ROOT.'/vendor/autoload.php';
 $app = Automation\Core\Application::instance();
 
 $app->run();
-
-class Dependency
-{
-    public function __toString(): string
-    {
-        return '55';
-    }
-}
-
-$resolved = $app->resolve(function (int $a, int $b, int|string $c, Dependency $dependency, int|string $d = 64) {
-    return $a + $b + $c + $d + (string) $dependency;
-}, ['b' => 3, 'a' => 1, 'a' => 2, 'c' => '4']);
-
-dump($resolved);
