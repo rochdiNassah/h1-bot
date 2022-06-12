cfc7631702a97c0a406e6e18a1002b138ca51f7d
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-12 18:06:57 +0100

    Filesystem test

--- tests/Core/FilesystemTest.php
+++ tests/Core/FilesystemTest.php
@@ -6,8 +6,29 @@ use Automation\Core\Filesystem;
 
 final class FilesystemTest extends TestCase
 {
-    public function test_is_exists(): void
+    public function test_path_is_exists(): void
     {
-        dump((string) app(Filesystem::class));
+        $fs = app(Filesystem::class);
+
+        fopen((string) $fs->to('tests/Core/foo'), 'x');
+
+        $this->assertTrue($fs->to('tests/Core')->exists('foo'));
+
+        unlink((string) $fs->to('tests/Core/foo'));
+
+        $this->assertFalse($fs->to('tests/Core')->exists('foo'));
+    }
+
+    public function test_path_is_missing(): void
+    {
+        $fs = app(Filesystem::class);
+
+        fopen((string) $fs->to('tests/Core/bar'), 'x');
+
+        $this->assertFalse($fs->to('tests/Core')->missing('bar'));
+
+        unlink((string) $fs->to('tests/Core/bar'));
+
+        $this->assertTrue($fs->to('tests/Core')->missing('bar'));
     }
 }
