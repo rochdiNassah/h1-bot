597739bd06cd212d3daa9a98e379fb83ffba53b3
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-13 17:52:04 +0100

    ServerBag

--- src/Core/Http/Request.php
+++ src/Core/Http/Request.php
@@ -4,11 +4,19 @@ namespace Automation\Core\Http;
 
 use Automation\Core\Application;
 use Automation\Interfaces\RequestInterface;
+use Automation\Core\Http\Bags\ServerBag;
 
 class Request implements RequestInterface
 {
+    public function __construct(
+        private Application $app,
+        private ServerBag $server
+    ) {
+
+    }
+
     public function foo()
     {
-        dump(__FUNCTION__);
+        dump($this->server->all());
     }
 }
