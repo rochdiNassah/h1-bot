638a4edda5d5debc829796f7c1699529188f600e
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-22 19:18:45 +0100

    MonitorPacketsCommand

--- commands.php
+++ commands.php
@@ -4,6 +4,7 @@ use Automation\Core\Facades\Console;
 use Automation\App\Commands\Encoding\{EncodeCommand, DecodeCommand, DetectEncodingCommand, SplitJWTCommand};
 use Automation\App\Commands\Misc\StrlenCommand;
 use Automation\App\Commands\Bruteforcing\BruteforceJWTSecretKeyCommand;
+use Automation\App\Commands\Alerts\MonitorPacketsCommand;
 
 Console::add(app(EncodeCommand::class));
 Console::add(app(DecodeCommand::class));
@@ -11,3 +12,4 @@ Console::add(app(DetectEncodingCommand::class));
 Console::add(app(SplitJWTCommand::class));
 Console::add(app(StrlenCommand::class));
 console::add(app(BruteforceJWTSecretKeyCommand::class));
+Console::add(app(MonitorPacketsCommand::class));
---
+++ src/App/Commands/Alerts/MonitorPacketsCommand.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Automation\App\Commands\Alerts;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputInterface, InputOption};
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Automation\Core\Facades\Encoder;
+
+#[AsCommand(
+    name: 'monitor-packets'
+)]
+class MonitorPacketsCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setHelp('Monitor incoming packets.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        return Command::SUCCESS;
+    }
+}
