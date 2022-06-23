f88dffb555cfab0b73991c4794dcbb7012456565
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-23 14:33:21 +0100

    Commands

--- commands.php
+++ commands.php
@@ -2,15 +2,9 @@
 
 use Automation\Core\Facades\Console;
 use Automation\App\Commands\Encoding\{EncodeCommand, DecodeCommand, DetectEncodingCommand, SplitJWTCommand};
-use Automation\App\Commands\Misc\{StrlenCommand, ShellServerCommand};
-use Automation\App\Commands\Bruteforcing\BruteforceJWTSecretKeyCommand;
-use Automation\App\Commands\Alerts\MonitorPacketsCommand;
+
 
 Console::add(app(EncodeCommand::class));
 Console::add(app(DecodeCommand::class));
 Console::add(app(DetectEncodingCommand::class));
 Console::add(app(SplitJWTCommand::class));
-Console::add(app(StrlenCommand::class));
-Console::add(app(BruteforceJWTSecretKeyCommand::class));
-Console::add(app(MonitorPacketsCommand::class));
-Console::add(app(ShellServerCommand::class));
