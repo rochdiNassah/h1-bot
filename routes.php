efd7fb509564ebf78fbb7dae287198386fd396a6
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-25 03:12:36 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -6,6 +6,3 @@ use Automation\App\Controllers\StringManipulationController;
 Router::get('/', function () {
     return View::make('homepage');
 });
-
-Router::get('/string-manipulation', [StringManipulationController::class, 'index']);
-Router::post('/string-manipulation', [StringManipulationController::class, 'eval']);
