64e2c1fc6c51fb283066ed3b2ae1d84800f823e7
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-25 10:17:45 +0100

    Homepage view

--- views/homepage.php
+++ views/homepage.php
@@ -3,13 +3,13 @@
 <div class="container mx-auto flex justify-center">
     <div class="w-full xl:w-2/5 mt-32 flex justify-center flex-wrap space-y-8">
         <div class="w-full container mx-auto">
-            <img class="w-1/2 md:w-1/3 mx-auto" src="<?= asset('logo.svg') ?>" />
+            <img class="w-1/2 sm:w-60 mx-auto" src="<?= asset('logo.svg') ?>" />
         </div>
 
-        <h1 class="text-lg sm:text-2xl md:text-3xl text-gray-600 font-bold text-center">Maintain bug-bounty targets automatically!</h1>
+        <h1 class="text-lg sm:text-2xl text-gray-600 font-bold text-center">Maintain bug-bounty targets automatically!</h1>
 
         <form class="w-full px-4" action="#" method="post">
-            <input class="w-full appearance-none outline-none p-4 border border-gray-200" placeholder="Exact target name..." name="target" autocomplete="off">
+            <input class="w-full appearance-none outline-none p-4 text-xs border border-gray-200" placeholder="Exact target name..." name="target" autocomplete="off">
         </form>
     </div>
 </div>
