861b8c35e69dc0c3a21cb943ed4ba3e9641d16b7
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-25 03:14:35 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types=1);
 
 use Automation\Core\Facades\{Router, View};
-use Automation\App\Controllers\StringManipulationController;
 
 Router::get('/', function () {
     return View::make('homepage');
