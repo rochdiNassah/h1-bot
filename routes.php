f7a47e199ab0ebe5259ca89cfa68fe4d24096d78
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-27 02:30:12 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -4,12 +4,21 @@ use Automation\Framework\Facades\Router;
 use App\Controllers\ProgramController;
 use App\Controllers\DebugController;
 
+Router::view('/', 'homepage');
+Router::post('/', [ProgramController::class, 'get']);
+
+Router::get('programs', [ProgramController::class, 'all']);
+
+Router::view('/programs/add', 'program.add');
+Router::post('/programs/add', [ProgramControler, 'add']);
+
+Router::get('/programs/{id}/update', [ProgramController::class, 'updateView']);
+Router::post('/programs/{id}/update', [ProgramControler, 'update']);
+
+Router::get('/programs/{id}/delete', [ProgramControler, 'delete']);
+
 Router::get('/debug', [DebugController::class, 'get']);
 Router::post('/debug', [DebugController::class, 'post']);
 Router::head('/debug', [DebugController::class, 'head']);
+Router::delete('/debug', [DebugController::class, 'delete']);
 Router::options('/debug', [DebugController::class, 'options']);
-
-Router::view('/', 'homepage');
-Router::view('/program/add', 'program.add');
-
-Router::post('/', [ProgramController::class, 'get']);
