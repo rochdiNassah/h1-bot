ef88e438a245c9c6ca66c0ac3ea05b51e9905195
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-28 03:42:22 +0100

    Update ViewFactory.php

--- Framework/View/ViewFactory.php
+++ Framework/View/ViewFactory.php
@@ -2,14 +2,11 @@
 
 namespace Automation\Framework\View;
 
-use Automation\Framework\Application;
-use Automation\Framework\Facades\Application as ApplicationFacade;
-
 class ViewFactory
 {
     public static function make(string $view, array $data = []): string
     {
-        $view = ApplicationFacade::resolve(View::class, [$view, $data]);
+        $view = app(View::class, [$view, $data]);
 
         $view->render();
 
