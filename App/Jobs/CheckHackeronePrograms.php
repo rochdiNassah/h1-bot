1e9dd805a63ab3c2dd9a6f7a9e8093bf0718eaae
Rochdi Nassah <rochdinassah.1998@gmail.com>
2022-06-29 18:00:02 +0100

    CheckHackeronePrograms

--- App/Jobs/CheckHackeronePrograms.php
+++ App/Jobs/CheckHackeronePrograms.php
@@ -6,7 +6,9 @@ use Automation\Framework\Application;
 use Automation\Framework\Interfaces\JobInterface;
 use Automation\Framework\Notifications\Slack;
 use Automation\Framework\Facades\Client;
+use Automation\Framework\Facades\Filesystem;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
 
 class CheckHackeronePrograms implements JobInterface
 {
@@ -16,22 +18,45 @@ class CheckHackeronePrograms implements JobInterface
         private Application $app
     ) {
         $client_options = [
-            'base_uri' => 'https://hackerone.com'
+            'base_uri' => 'https://hackerone.com',
+            'timeout'  => 10
         ];
 
         $app->bind(Client::class, app(GuzzleClient::class, [$client_options]));
     }
 
-    public function __invoke(Slack $slack): bool
+    public function __invoke(Slack $slack)
     {
-        $response = Client::request('GET', '/directory/programs');
+        $date_format = 'Y-m-d\TH:i:s\Z';
 
-        dump((string) $response->getStatusCode());
+        $current_date = new \DateTime(date(DATE_RFC3339));
 
-        if (false !== $response) {            
-            return true;
+        $current_date->modify('-30 days');
+
+        $json_request = (array) json_decode(file_get_contents(Filesystem::to('resources/json/requests/hackerone/directory.json')));
+
+        $response = Client::request('POST', '/graphql', ['json' => $json_request]);
+
+        if (!$response) {
+            throw new \Exception();
         }
 
-        throw new Exception('I failed');
+        $json_response = json_decode((string) $response->getBody());
+
+        $programs = $json_response->data->teams->edges;
+
+        foreach ($programs as $program) {
+            $name        = $program->node->name;
+            $handle      = $program->node->handle;
+            $launched_at = (new \DateTimeImmutable($program->node->launched_at));
+
+            $current_date->setTimeZone($launched_at->getTimezone());
+
+            if ($launched_at > $current_date) {
+                $slack->send(sprintf('(HackerOne) A new program has launched (%s)', $name));
+            }
+        }
+
+        return true;
     }
 }
